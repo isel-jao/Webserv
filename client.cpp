d06d1196d86b48ec2bebfe58609ddcee270a45f4
Issam El jaouhary <isel-jao@e1r4p10.1337.ma>
2021-11-01 14:57:45 +0100

    simple client/server communication

--- client.cpp
+++ client.cpp
@@ -1,48 +1,60 @@
-// Client side C/C++ program to demonstrate Socket programming
-// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
 #include <string.h>
-#define PORT 8080
 
+#define PORT 8080
 int main(int argc, char const *argv[])
 {
-	int sock = 0, valread;
-	struct sockaddr_in serv_addr;
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    // Only this line has been changed. Everything is same.
+    char hello[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("\n Socket creation error \n");
-		return -1;
-	}
-
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-
-	// Convert IPv4 and IPv6 addresses from text to binary form
-	if (inet_pton(AF_INET, "10.11.4.10", &serv_addr.sin_addr) <= 0)
-	{
-		printf("\nInvalid address/ Address not supported \n");
-		return -1;
-	}
-
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{
-		printf("\nConnection Failed \n");
-		return -1;
-	}
-	while (1)
-	{
-		char buffer[1024] = {0};
-		printf("you:\t");
-		scanf(" %[^\n]", buffer);
-		send(sock, buffer, strlen(buffer), 0);
-		memset(buffer, 0 ,1024);
-		valread = read(sock, buffer, 1024);
-		printf("server: %s\n", buffer);
-	}
-	close(sock);
-	return 0;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , hello , strlen(hello));
+        printf("------------------Hello message sent-------------------");
+        close(new_socket);
+    }
+    return 0;
 }
--- srcs/server.cpp
+++ srcs/server.cpp
@@ -1,62 +1,60 @@
-#include <stdio.h> 
-#include <string.h>   //strlen 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <unistd.h>   //close 
-#include <arpa/inet.h>    //close 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
-     
-#define TRUE   1 
-#define FALSE  0 
-#define PORT 8888 
+// Server side C program to demonstrate HTTP Server programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
+#define PORT 8080
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
-
+	int server_fd, new_socket;
+	long valread;
 	struct sockaddr_in address;
 	int addrlen = sizeof(address);
+
+	// Only this line has been changed. Everything is same.
+	char hello[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 
 	// Creating socket file descriptor
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
-		perror("socket failed");
+		perror("In socket");
 		exit(EXIT_FAILURE);
 	}
 
 	address.sin_family = AF_INET;
-	// address.sin_addr.s_addr = inet_addr("10.11.4.10") for this machine
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 
-	// Forcefully attaching socket to the port 8080
+	memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
 	{
-		perror("bind failed");
+		perror("In bind");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0)
+	if (listen(server_fd, 10) < 0)
 	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-	{
-		perror("accept");
+		perror("In listen");
 		exit(EXIT_FAILURE);
 	}
 	while (1)
 	{
-		char buffer[1024] = {0};
-		valread = read(new_socket, buffer, 1024);
-		printf("client:\t%s\nyou:\t", buffer);
-		memset(buffer, 0, 1024);
-		scanf(" %[^\n]", buffer);
-		send(new_socket, buffer, strlen(buffer), 0);
-	}
+		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+		{
+			perror("In accept");
+			exit(EXIT_FAILURE);
+		}
 
+		char buffer[30000] = {0};
+		valread = read(new_socket, buffer, 30000);
+		printf("%s\n", buffer);
+		write(new_socket, hello, strlen(hello));
+		printf("------------------Hello message sent-------------------");
+		close(new_socket);
+	}
 	return 0;
 }
